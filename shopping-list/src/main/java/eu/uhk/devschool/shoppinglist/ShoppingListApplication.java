e4c6ce364abea0d94cca9c2d6ff268a9563a5b78
Radek Boukal <radek.boukal@unicorn.com>
2018-07-10 13:19:19 +0200

    init commit

---
+++ shopping-list/src/main/java/eu/uhk/devschool/shoppinglist/ShoppingListApplication.java
@@ -0,0 +1,12 @@
+package eu.uhk.devschool.shoppinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListApplication.class, args);
+	}
+}
---
+++ shopping-list/src/test/java/eu/uhk/devschool/shoppinglist/ShoppingListApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.uhk.devschool.shoppinglist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShoppingListApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
